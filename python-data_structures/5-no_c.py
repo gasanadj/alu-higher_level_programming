55638016ddba679cf8e7ef2432156acbabf8a691
gasanadj <d.gasana@alustudent.com>
2023-03-07 07:51:33 -0800

    ft: Python data structure projects

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def no_c(my_string) :
+def no_c(my_string):
     length = len(my_string)
     j = 0
     new_string = my_string[:]
--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]) :
+def max_integer(my_list=[]):
     length = len(my_list)
     if length == 0:
         return (None)
