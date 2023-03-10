e91c4f460383282489067b729379e790b70dcc3f
gasanadj <d.gasana@alustudent.com>
2023-03-10 06:53:16 -0800

    fixing zero

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    func = 0
-    for in in range(x):
+    element = 0
         try:
-            print("{}".format(my_list[i]), end="")
-            func += 1
-        except IndexError:
-            break
+            for i in range(x):
+                print("{}".format(my_list[i]), end="")
+                func += 1
+        except:
+            pass
     print("")
-    return (func)
+    return element
